ff811b40c03f66696715f81d0202e31d84389be0
codec-abc <viot.camille@outlook.com>
2015-10-04 13:01:16 +0200

    adding FFI for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,29 @@
 extern crate hyper;
 extern crate libc;
 
-//use std::io::Write;
 use std::io::Read;
 use std::sync::Mutex;
-
 use hyper::Server;
 use hyper::server::Request;
 use hyper::server::Response;
 use hyper::server::Handler;
-
-//use std::ffi::CString;
+use libc::c_char;
+use std::ffi::CStr;
 use std::ptr;
+use std::mem;
+use std::sync::Arc;
+use std::thread;
 
 pub struct RustSrvHandler
 {
-    strs :  Mutex<Vec<String>>,
-}
-
-fn main()
-{
-    start(3000);
+    strs :  Arc<Mutex<Vec<String>>>,
 }
 
 impl Handler for RustSrvHandler
 {
     fn handle(& self, mut req: Request, res: Response)
     {
+        res.send(b"").unwrap();
         if req.method == hyper::method::Method::Post
         {
             let mut buffer = String::new();
@@ -37,38 +34,50 @@ impl Handler for RustSrvHandler
                 {
                     let mut x = self.strs.lock().unwrap();
                     x.push(buffer);
-                    /*
-                    for y in &(*x)
-                    {
-                        println!("{}", y);
-                    }
-                    println!("");
-                    */
                 }
                 Err(_) => {}
             }
         }
-        res.send(b"").unwrap();
     }
 }
 
-
 #[no_mangle]
-pub extern fn start(port : u32)
+pub extern fn start(port : u32) -> *const Mutex<Vec<String>>
 {
-    let h = RustSrvHandler { strs : Mutex::new(Vec::new())};
+    let h = RustSrvHandler { strs : Arc::new(Mutex::new(Vec::new())) };
+    let w = h.strs.clone();
     let s = String::from("0.0.0.0:") + &(*port.to_string());
-    Server::http(&(*s)).unwrap().handle(h).unwrap();
+    thread::spawn(move ||
+        {
+            Server::http(&(*s)).unwrap().handle(h).unwrap();
+        }
+    );
+    return &(*w) ;
 }
 
 #[no_mangle]
-pub extern fn get_string (h : RustSrvHandler) -> *const u8
+pub extern fn get_string (h : *const Mutex<Vec<String>>) -> *mut c_char
 {
-    let mut x = h.strs.lock().unwrap();
+    let mut x = unsafe { (*h).lock().unwrap() };
     let st = x.pop();
     match st
     {
-        None => { return ptr::null(); }
-        Some(y) => { return y.as_ptr(); }
+        None =>
+        {
+            let y : *mut i8 = ptr::null_mut();
+            return y;
+        }
+        Some(y) =>
+        {
+            let p = y.as_ptr();
+            mem::forget(y);
+            p as *mut _
+        }
     }
+}
+
+#[no_mangle]
+pub extern fn free_string(c : *mut c_char)
+{
+    let _ = unsafe { CStr::from_ptr(c) };
 }
